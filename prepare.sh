8ba54e5450fa272dd5c98e564c06ab8a276314b4
Hiroshi Miura <miurahr@linux.com>
2014-01-05 16:45:51 +0900

    add drush download reference(comment out)
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- prepare.sh
+++ prepare.sh
@@ -48,8 +48,8 @@ themes=(bootstrap-7.x-3.0.tar.gz  touch-7.x-1.7.tar.gz)
 
 cd ${DRUPAL_DIR}
 wget -c -N http://ftp.drupal.org/files/projects/drupal-7.24.tar.gz
-
 wget -c -N http://download.pear.php.net/package/Console_Table-1.1.3.tgz
+#wget -c -N https://github.com/drush-ops/drush/archive/6.2.0.tar.gz
 
 cd ${DRUPAL_DIR}/modules
 for item in ${modules[*]}
